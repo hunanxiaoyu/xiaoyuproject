be993cf06dc0b17226abb635e1442fc5a8342eba
shu <3613646170@qq.com>
2025-04-21 19:38:23 +0800

    完成了入库和出库、仓库调度、高德地图引入

--- src/main/java/com/logistics/controller/WareHouseController.java
+++ src/main/java/com/logistics/controller/WareHouseController.java
@@ -1,14 +1,17 @@
 package com.logistics.controller;
 
 import cn.hutool.core.lang.Console;
+import com.logistics.domain.dto.InventoryDto;
+import com.logistics.domain.dto.WarehouseStockDto;
+import com.logistics.domain.vo.WarehouseLocationVo;
+import com.logistics.domain.vo.WarehouseLogVo;
 import com.logistics.domain.vo.WarehouseStockVo;
 import com.logistics.service.WareHouseService;
 import com.logistics.utils.ResponseResult;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.math.BigInteger;
 import java.util.List;
 import java.util.Map;
 
@@ -24,8 +27,33 @@ public class WareHouseController {
       return ResponseResult.ok(allWareHouseStock);
   }
   @GetMapping("/getWareHouseStock/List")
-    public ResponseResult<List<WarehouseStockVo>> getWareHouseStock(){
-      List<WarehouseStockVo> warehouseStockVos= wareHouseService.WarehouseStock();
+    public ResponseResult<List<WarehouseStockVo>> getWareHouseStock(String productName,String status){
+      List<WarehouseStockVo> warehouseStockVos= wareHouseService.WarehouseStock(productName, status);
       return ResponseResult.ok(warehouseStockVos);
+  }
+  @PutMapping("/adjusting/inventory")
+    public ResponseResult<Void>  updateInventory(@RequestBody InventoryDto inventoryDto) {
+      wareHouseService.updateInventory(inventoryDto);
+      return ResponseResult.ok();
+  }
+  @PostMapping("/InOutInventoty")
+  public ResponseResult<Void>  updateInOutInventory( Integer id, String type, Integer quantity, String remark) {
+       wareHouseService.updateInOutInventory(id,type,quantity,remark);
+       return ResponseResult.ok();
+  }
+  @GetMapping("/getStockChangeRecord")
+    public ResponseResult<List<WarehouseLogVo>> getStockChangeRecord(){
+    List<WarehouseLogVo> stockChangeRecord= wareHouseService.getStockChangeRecord();
+    return ResponseResult.ok(stockChangeRecord);
+  }
+  @GetMapping("/getWarehouseLocation")
+  public  ResponseResult<List<WarehouseLocationVo>> getWarehouseLocation(Integer id){
+    List<WarehouseLocationVo> warehouseLocation= wareHouseService.getWarehouseLocation(id);
+    return ResponseResult.ok(warehouseLocation);
+  }
+  @PostMapping("/stockSchedule")
+    public ResponseResult<Void>  stockSchedule(@RequestBody WarehouseStockDto warehouseStockDto) {
+       wareHouseService.stockSchedule(warehouseStockDto);
+       return ResponseResult.ok();
   }
 }
--- src/main/java/com/logistics/service/WareHouseService.java
+++ src/main/java/com/logistics/service/WareHouseService.java
@@ -1,14 +1,30 @@
 package com.logistics.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.logistics.domain.dto.InventoryDto;
+import com.logistics.domain.dto.WarehouseStockDto;
 import com.logistics.domain.entity.WarehouseStock;
+import com.logistics.domain.vo.WarehouseLocationVo;
+import com.logistics.domain.vo.WarehouseLogVo;
 import com.logistics.domain.vo.WarehouseStockVo;
 
+import java.math.BigInteger;
 import java.util.List;
 import java.util.Map;
 
 public interface WareHouseService extends IService<WarehouseStock> {
     Map<String,Integer> getAllWareHouseStock();
 
-    List<WarehouseStockVo> WarehouseStock();
+    List<WarehouseStockVo> WarehouseStock(String productName,String status);
+
+    void updateInventory(InventoryDto inventtoryDto);
+
+
+    void updateInOutInventory(Integer id, String type, Integer quantity, String remark);
+
+    List<WarehouseLogVo> getStockChangeRecord();
+
+    List<WarehouseLocationVo> getWarehouseLocation(Integer id);
+
+    void stockSchedule(WarehouseStockDto warehouseStockDto);
 }
